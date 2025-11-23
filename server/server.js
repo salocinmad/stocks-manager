37b95c054a93a0f6e19d5b94e46d1c0368ef926e
salocinmad <nicolas.mad@gmail.com>
2025-11-23 01:55:34 +0100

    añadidos logs de deputarcion por fallo en login

--- server/server.js
+++ server/server.js
@@ -28,6 +28,12 @@ app.use(cors({
 }));
 app.use(express.json());
 
+// Global Request Logger
+app.use((req, res, next) => {
+  console.log(`📥 ${req.method} ${req.url}`);
+  next();
+});
+
 // Conectar a la base de datos
 connectDB();
 
